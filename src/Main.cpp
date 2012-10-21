fcb3d49f4732ecc07515d1efa3af4cb8467fee29
Suzana <swzysantos@gmail.com>
2012-10-21 18:16:25 -0200

    Só começando a mexer com keypoints

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,19 +13,24 @@ using namespace cv;
 
 int main( int argc, char** argv ) {
     Mat image;
-    image = imread( argv[1], 1 );
+    vector<KeyPoint> keypoints;
 
-    Ptr<FeatureDetector> detector = FeatureDetector::create("SURF");
-
-    if( argc != 2 || !image.data ) {
-      printf( "No image data \n" );
+    if (argc != 2) {
+      printf( "Uso:\n./%s nome_da_imagem\n", argv[1]);
       return -1;
     }
 
-    namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
-    imshow( "Display Image", image );
+    image = imread( argv[1], 1 );
+    if (!image.data) {
+        printf("Erro ao abrir arquivo %s", argv[1]);
+        return -1;
+    }
 
-    waitKey(0);
+    Ptr<FeatureDetector> detector = FeatureDetector::create("SURF");
 
-    return 0;
+    /* TODO Em algum momento, faremos a extração dos keypoints: */
+    detector->detect(image, keypoints);
+
+   return 0;
 }
+
